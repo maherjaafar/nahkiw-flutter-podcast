44e7b38a4e72e5f79a92b1f54c3ecdcc58b1b856
Maher Jaafar <34738373+maherjaafar@users.noreply.github.com>
2024-02-17 23:45:33 +0100

    Implement open email app with the plugin

--- lib/src/features/authentication/presentation/pages/confirmation_email_sent_page.dart
+++ lib/src/features/authentication/presentation/pages/confirmation_email_sent_page.dart
@@ -1,12 +1,12 @@
 import 'package:first_episode/src/src.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:open_mail_app/open_mail_app.dart';
 
-class ConfirmationEmailSentPage extends ConsumerWidget {
+class ConfirmationEmailSentPage extends StatelessWidget {
   const ConfirmationEmailSentPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final textTheme = context.textTheme;
     return Scaffold(
       appBar: AppBar(),
@@ -58,7 +58,29 @@ class ConfirmationEmailSentPage extends ConsumerWidget {
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(width * 0.7, 50),
                   ),
-                  onPressed: () {},
+                  onPressed: () async {
+                    // Android: Will open mail app or show native picker.
+                    // iOS: Will open mail app if single mail app found.
+                    var result = await OpenMailApp.openMailApp();
+
+                    // If no mail apps found, show error
+                    if (!result.didOpen && !result.canOpen) {
+                      showNoMailAppsDialog(context);
+
+                      // iOS: if multiple mail apps found, show dialog to select.
+                      // There is no native intent/default app system in iOS so
+                      // you have to do it yourself.
+                    } else if (!result.didOpen && result.canOpen) {
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return MailAppPickerDialog(
+                            mailApps: result.options,
+                          );
+                        },
+                      );
+                    }
+                  },
                   child: const Text('Open Email App'),
                 ),
                 FirstEpisodeClickableText(
@@ -73,6 +95,26 @@ class ConfirmationEmailSentPage extends ConsumerWidget {
           );
         },
       ),
+    );
+  }
+
+  void showNoMailAppsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Open Mail App"),
+          content: const Text("No mail apps installed"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
